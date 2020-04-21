f6d8803e6a44200aa5efce1fee1017d09869bdb3
Paul Trampert <paul.trampert@ptrampert.com>
2020-04-21 17:51:43 -0400

    Refactor CacheBustedFile into its own file.

--- PTrampert.Webpack.CacheBuster/CacheBustTagHelper.cs
+++ PTrampert.Webpack.CacheBuster/CacheBustTagHelper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
@@ -53,55 +52,6 @@ namespace PTrampert.Webpack.CacheBuster
                 output.Attributes.SetAttribute(output.TagName == "script" ? "src" : "href",
                     $"{cachedFile.WebPath}?v={cachedFile.GetHash(webroot)}");
             }
-        }
-    }
-
-    public class CacheBustedFile
-    {
-        private string hash;
-        public string WebPath { get; set; }
-        public DateTimeOffset? LastChecked { get; set; }
-
-        public CacheBustedFile(string webPath)
-        {
-            WebPath = webPath;
-        }
-
-        public DateTimeOffset? GetLastModified(IFileProvider webroot)
-        {
-            return webroot.GetFileInfo(WebPath)?.LastModified;
-        }
-
-        public bool Exists(IFileProvider webroot)
-        {
-            return webroot.GetFileInfo(WebPath)?.Exists ?? false;
-        }
-
-        public bool IsModified(IFileProvider webroot)
-        {
-            var lastModified = GetLastModified(webroot);
-            return string.IsNullOrWhiteSpace(hash) 
-                   || !lastModified.HasValue 
-                   || !LastChecked.HasValue 
-                   || lastModified.Value > LastChecked.Value;
-        }
-
-        public string GetHash(IFileProvider webroot)
-        {
-            if (IsModified(webroot))
-            {
-                var fileInfo = webroot.GetFileInfo(WebPath);
-                using (var sha265 = SHA256.Create())
-                using (var stream = fileInfo.CreateReadStream())
-                {
-                    var rawHash = sha265.ComputeHash(stream);
-                    hash = Convert.ToBase64String(rawHash)
-                        .Replace('+', '-')
-                        .Replace('/', '_');
-                }
-            }
-
-            return hash;
         }
     }
 }
