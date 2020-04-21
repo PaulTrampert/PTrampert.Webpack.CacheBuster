cffa8355463460353d1603c099d0585c8e8900ff
Paul Trampert <paul.trampert@ptrampert.com>
2020-04-21 18:19:54 -0400

    Add service collection convenience method.

---
+++ PTrampert.Webpack.CacheBuster/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using PTrampert.Webpack.CacheBuster;
+
+namespace Microsoft.Extensions.DependencyInjection.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCacheBusting(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            return services.AddSingleton<ICacheBuster, CacheBuster>();
+        }
+    }
+}
